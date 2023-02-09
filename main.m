eddd777bec911105e74e0d82d70913554e190297
HolmanDev <18399006+HolmanDev@users.noreply.github.com>
2023-02-09 11:05:12 +0100

    list and rank factors

---
+++ main.m
@@ -0,0 +1,9 @@
+% 1) vattenmängd, masscentrum, tryckcentrum, stabilitet, flaskhalsens area,
+% tryck i flaska, vinkel, gravitation, vattnets densitet, lutning av
+% vattnet
+% 2) luftmotstånd, vattentemp, lufttemp i flaskan, ljudhastighet för
+% vattenånga (bara viktigt om vattnet flödar supersnabbt ut ur raketen)
+% 3) vindriktning, lufttryck, jordens rotation, lufttemp utanför flaskan
+
+% Vi ska nu skapa en modell som beräknar hur långt vår vattenraket kommer
+% åka, givet en viss vattenmängd. Mer rätt och mer långt = bättre.
