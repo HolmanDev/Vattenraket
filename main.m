eb5755164a2a84198b5fbe758789b84aba4d9c71
HolmanDev <18399006+HolmanDev@users.noreply.github.com>
2023-02-22 20:08:40 +0100

    add massflow formula

--- main.m
+++ main.m
@@ -2,13 +2,22 @@
 g = 9.82; % [m/s^2]
 density_water = 997.13; % [kg/m^3]
 kelvin_celsius_offset = 273.15;
+p_0 = 101300; % [N/m^2]
 
 % Variabler
 angle = 45; % Uppskjutningsvinkel [deg]
 p = 100; % Lufttryck i raketen [N/m^2]
 T_C = 20; % [C]
-T_K = temperature_C + kelvin_celsius_offset; % [K]
-m_raket; % Raketmassa [kg]
+T_K = T_C + kelvin_celsius_offset; % [K]
+m_raket = 0.1; % Raketmassa [kg]
+r_nozzle = 0.01025; % Mynningsradie [m]
+d_nozzle = 2*r_nozzle; % Mynningsdiameter [m]
+A_nozzle = r_nozzle * r_nozzle * pi; % Mynningsarea [m^2]
+cd = 1; % "Discharge coefficient [dimensionslös]
+p_air = 300000; %! MÅSTE RÄKNAS UT [N/m^2]
+
+% Räkna ut massflöde genom mynningen
+m_flow = A*Cd*sqrt(2*density_water*(p_air-p_0));
 
 % Uträkning
 speed0 = 10; % räkna ut sen
