38692b9dc658130a4ca82af334e3b4418099dcbb
HolmanDev <18399006+HolmanDev@users.noreply.github.com>
2023-03-02 22:00:07 +0100

    progress

--- Velocity.m
+++ Velocity.m
@@ -1,33 +1,38 @@
-function [v_vec] = Velocity(N, dt, m_rocket, m_fuel, g, p_0, p_air, density_water, A_nozzle, cd)
+function [v_vec, m_flow_vec] = Velocity(N, dt, V_air_0, m_rocket, m_fuel, g, p_0, p_air, density_water, A_nozzle, C_discharge)
     v_vec(:, 1) = [0; 0];
     m_0 = m_rocket + m_fuel;
-    a_g = [0; -g];
     m_e = 0;
-    v_e = [0; 0];
-    local_up = 1/sqrt(2)*[1; 1]; % Should pitch over time
-    %m_flow_vec = zeros(1, N);
+    local_up = 1/sqrt(2)*[1; 1]; % Borde "pitcha" över tid
+    m_flow_vec = zeros(1, N);
     for i=1:N
-        delta_p = p_air-p_0; % p_air should decrease
+        V_air = V_air_0 + m_e / density_water;
+        p = p_air * V_air_0/V_air;
+        delta_p = p - p_0; % p_air borde minska
+        if mod(i,100) == 0
+            %disp(delta_p/100000)
+        end
+        
+        % Exhaust-hastighet. OBS: Relativ till raketen!  
+        beta = 20.5/88.1; % flaskhalsdiameter / flaskdiameter
+        v_e = C_discharge * sqrt(2*delta_p / (density_water * (1 - beta^4))) * -local_up;
 
-        v_e_last = v_e;
-        v_e = cd * sqrt(2*delta_p/density_water) * -local_up; % Relative to rocket!
-        a_e = (v_e - v_e_last) ./ dt;
-
+        % Massflöde       
         m_flow = norm(v_e) * density_water * A_nozzle;
-        %m_flow_vec(i) = m_flow;
         m_e = m_e + m_flow*dt;
         is_empty = m_e >= m_fuel;
         if is_empty
             m_e = m_fuel;
             m_flow = 0;
         end
+        m_flow_vec(i) = m_flow;
 
-        a_luft = [0; 0]; % Ändra sen
+        % Krafter
+        F_prop = -m_flow*v_e;
+        F_g = [0; -g] * (m_0 - m_e);
+        F_air = -0.4 * 1.225 * 0.5 * 0.04405^2 * pi * norm(v_vec(:, i)) * v_vec(:, i); % Ändra sen
 
         % Euler framåt
-        v_vec(:, i+1) = v_vec(:, i) + dt*(m_flow*(-v_e) - m_e*a_e)...
-            ./ (m_0 - m_e) + a_g*dt + a_luft*dt;
+        a = (F_prop + F_g + F_air) ./ (m_0 - m_e);
+        v_vec(:, i+1) = v_vec(:, i) + dt*a;
     end
-    %figure(5)
-    %plot(1:N, m_flow_vec);
 end
--- main.m
+++ main.m
@@ -1,73 +1,95 @@
-% Konstanter
-g = 9.82; % [m/s^2]
-density_water = 997.13; % [kg/m^3]
-kelvin_celsius_offset = 273.15;
-p_0 = 101300; % [N/m^2]
+volumes = [0.0005, 0.00075, 0.001, 0.00125, 0.0015];
 
-% Variabler
-V_water = 0.001; % 1 liter [m^3]
-angle = 45; % Uppskjutningsvinkel [deg]
-T_C = 20; % [C]
-T_K = T_C + kelvin_celsius_offset; % [K]
-m_rocket = 0.1; % Raketmassa [kg]
-r_nozzle = 0.01025; % Mynningsradie [m]
-d_nozzle = 2*r_nozzle; % Mynningsdiameter [m]
-A_nozzle = r_nozzle * r_nozzle * pi; % Mynningsarea [m^2]
-cd = 1; % "Discharge coefficient [dimensionslös]
-p_air = 300000; %! MÅSTE RÄKNAS UT [N/m^2]
-
-% Hastighet
-N = 70000;
-dt = 0.0001;
-v_vec = Velocity(N, dt, m_rocket, m_fuel, g, p_0, p_air, density_water, A_nozzle, cd);
-t_vec = 0:dt:(N*dt);
-figure(1)
-plot(t_vec, v_vec(2, :)); % v_y over time
-title("Velocity")
-xlabel("t [s]")
-ylabel("v_y [m/s]")
-hold on;
-yline(0);
-hold off
-
-% First seconds velocity
-figure(2)
-v_y_vec = v_vec(2, :);
-T = 0.5;
-plot(t_vec(1:T/dt), v_y_vec(1:T/dt)); % v_y over time
-title("Velocity", "first " + T + " seconds")
-xlabel("t [s]")
-ylabel("v_y [m/s]")
-hold on;
-yline(0);
-hold off
-
-% Position
-s_vec = [0;0];
-for i=1:N
-    s_vec(:, i+1) = s_vec(:, i) + v_vec(:, i)*dt;
+for v_i = 1:5
+    % Konstanter
+    g = 9.82; % [m/s^2]
+    density_water = 997.13; % [kg/m^3]
+    kelvin_celsius_offset = 273.15;
+    p_0 = 101300; % [N/m^2]
+    V_bottle = 0.0016; % [m^3]
+    
+    % Variabler
+    p_air_0 = 800000;
+    V_water = volumes(v_i); % [m^3]
+    V_air_0 = V_bottle - V_water;
+    angle = 45; % Uppskjutningsvinkel [deg]
+    m_rocket = 0.2; % Raketmassa [kg]
+    m_fuel = V_water * density_water;
+    T_C = 20; % [C]
+    T_K = T_C + kelvin_celsius_offset; % [K]
+    r_nozzle = 0.01025; % Mynningsradie [m]
+    d_nozzle = 2*r_nozzle; % Mynningsdiameter [m]
+    A_nozzle = r_nozzle * r_nozzle * pi; % Mynningsarea [m^2]
+    C_discharge = 0.5; % "Discharge coefficient [dimensionslös]
+    
+    % Hastighet
+    N = 90000;
+    dt = 0.0001;
+    [v_vec, m_flow_vec] = Velocity(N, dt, V_air_0, m_rocket, m_fuel, g, p_0, p_air_0, density_water, A_nozzle, C_discharge);
+    t_vec = 0:dt:(N*dt);
+    figure(1)
+    hold on;
+    plot(t_vec, v_vec(2, :)); % v_y over time
+    title("Velocity")
+    xlabel("t [s]")
+    ylabel("v_y [m/s]")
+    yline(0, 'HandleVisibility', 'off');
+    hold off
+    
+    % First seconds velocity
+    figure(2)
+    hold on;
+    v_y_vec = v_vec(2, :);
+    T = 0.5;
+    Tdt = round(T/dt);
+    plot(t_vec(1:Tdt), v_y_vec(1:Tdt)); % v_y over time
+    title("Velocity", "first " + T + " seconds")
+    xlabel("t [s]")
+    ylabel("v_y [m/s]")
+    yline(0, 'HandleVisibility', 'off');
+    hold off
+    
+    % Position
+    s_vec = [0;0];
+    for i=1:N
+        s_vec(:, i+1) = s_vec(:, i) + v_vec(:, i)*dt;
+    end
+    figure(3)
+    hold on
+    plot(s_vec(1, :), s_vec(2, :));
+    title("Position")
+    xlabel("x [m]")
+    ylabel("y [m]")
+    yline(0, 'HandleVisibility', 'off');
+    hold off
+    
+    % Acceleration
+    a_vec = zeros(2, N-1);
+    for i=1:(N-1)
+        a_vec(:, i) = (v_y_vec(:, i+1) - v_y_vec(:, i))/dt;
+    end
+    figure(4)
+    hold on
+    a_t_vec = t_vec(1:N-1);
+    a_y_vec = a_vec(2, :);
+    plot(a_t_vec(1:Tdt), a_y_vec(1:Tdt)); % v_y over time
+    title("Acceleration", "first " + T + " seconds")
+    xlabel("t [s]")
+    ylabel("a_y [m/s^2]")
+    yline(0, 'HandleVisibility','off');
+    hold off
+    
+    % Mass flow
+    figure(5)
+    hold on
+    plot(t_vec(1:Tdt), m_flow_vec(1:Tdt));
+    title("Mass flow", "first " + T + " seconds")
+    xlabel("t [s]")
+    ylabel("dm/dt [kg/s]")
+    hold off
 end
-figure(3)
-plot(s_vec(1, :), s_vec(2, :));
-title("Position")
-xlabel("x [m]")
-ylabel("y [m]")
-hold on
-yline(0);
-hold off
 
-% Acceleration
-a_vec = zeros(2, N-1);
-for i=1:(N-1)
-    a_vec(:, i) = (v_y_vec(:, i+1) - v_y_vec(:, i))/dt;
+for i=1:5
+    figure(i)
+    legend(strtrim(cellstr(num2str(volumes'))')) % Color mismatch?
 end
-figure(4)
-a_t_vec = t_vec(1:N-1);
-a_y_vec = a_vec(2, :);
-plot(a_t_vec(1:(T/dt)), a_y_vec(1:(T/dt))); % v_y over time
-title("Acceleration", "first " + T + " seconds")
-xlabel("t [s]")
-ylabel("a_y [m/s^2]")
-hold on;
-yline(0);
-hold off
